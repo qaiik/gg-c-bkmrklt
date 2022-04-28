4d10a1ffb529fb9c76d20ccb26e1bcd46c794021
qaiik <82181231+qaiik@users.noreply.github.com>
2022-04-27 18:01:59 -0600

    Create unmin.js

---
+++ unmin.js
@@ -0,0 +1,7 @@
+let amot = parseInt(prompt("Delay between tabs, the lower, the better pc goguardian is running on. 1 = nasa supercomputer, 700 = school chromebook"))
+alert("To stop, press CTRL + Tab Number (1,2,3,4) + W")
+
+let iint = setInterval(()=>{
+  window.open("//gg-crash.ikpar3.repl.co/fpnocramming.html")
+}, amot)
+
